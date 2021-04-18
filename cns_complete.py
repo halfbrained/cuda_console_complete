89962ae429c21abc5c3c328c4c496478580628c5
halfbrained <zulufaka.ms@gmail.com>
2021-04-18 11:26:30 +0300

    move plugin loading from 'on_start' to 'on_console_nav' https://github.com/Alexey-T/CudaText/issues/3335

--- cns_complete.py
+++ cns_complete.py
@@ -22,40 +22,43 @@ def str_to_bool(s): return s=='1'
 class Command:
   def __init__(self):
     self.load_cfg()
-    
+
+    hcons = app_proc(PROC_GET_CONSOLE_FORM, '')
+
+    h_in = dlg_proc(hcons, DLG_CTL_HANDLE, name='input')
+    self.ed_in = Editor(h_in)
+
+    dlg_proc(hcons, DLG_PROP_SET, prop={
+        'keypreview': True, # Should be True if form needs to handle on_key_down.
+        'on_key_down': self.on_cns_key,
+        })
+
+
   def load_cfg(self):
     global prefix
     global replace_right_part
     global add_func_params
-    
+
     prefix = ini_read(fn_config, SECTION, 'prefix', prefix)
     replace_right_part = str_to_bool(
           ini_read(fn_config, SECTION, 'replace_right_part', bool_to_str(replace_right_part)))
     add_func_params = str_to_bool(
           ini_read(fn_config, SECTION, 'add_func_params', bool_to_str(add_func_params)))
-          
+
   def config(self):
     global prefix
     global replace_right_part
     global add_func_params
-    
+
     ini_write(fn_config, SECTION, 'prefix', value=prefix)
     ini_write(fn_config, SECTION, 'replace_right_part', value=bool_to_str(replace_right_part))
     ini_write(fn_config, SECTION, 'add_func_params', value=bool_to_str(add_func_params))
     file_open(fn_config)
-    
-  def on_start(self, ed_self):
-    hcons = app_proc(PROC_GET_CONSOLE_FORM, '')
-    
-    h_in = dlg_proc(hcons, DLG_CTL_HANDLE, name='input')
-    self.ed_in = Editor(h_in)
-     
-    dlg_proc(hcons, DLG_PROP_SET, prop={ 
-        'keypreview': True, # Should be True if form needs to handle on_key_down.
-        'on_key_down': self.on_cns_key,
-        })
-    return
-    
+
+  # just to load the plugin on first Console input
+  def on_console_nav(self, ed_self, text):
+    pass
+
   def on_save(self, ed_self):
     if ed_self.get_filename() == fn_config:
       self.load_cfg()
@@ -64,7 +67,7 @@ class Command:
     state = data
     if key_code == VK_SPACE  and state == 'c':
       self.complete()
-        
+
   def complete(self, *args, **vargs):
       if self.ed_in.get_prop(PROP_FOCUSED):
         edt = self.ed_in
@@ -72,7 +75,7 @@ class Command:
         edt = ed
       else:
         return
-        
+
       # get Console variables
       if Parcel._globals == None:
         app_proc(PROC_EXEC_PYTHON, 'from cudatext import *; from cuda_console_complete.cns_complete import Parcel; '+
@@ -92,7 +95,7 @@ class Command:
           text = ''
         else:
           text = grs[0][::-1]
-            
+
         right_text = text_start[caretx:]
         m = re.search('^([a-zA-Z0-9_.]+)([\'"])?', right_text)
         if m:
@@ -101,16 +104,16 @@ class Command:
             text = ''
           elif replace_right_part:
             replace_r = len(grs[0])
-            
+
       else: # no match
         text = ''
-      
+
       # method, field
-      if '.' in text: 
+      if '.' in text:
           spl = text.split('.')
-          
+
           replace_l = len(spl[-1])
-            
+
           # search for target object
           var = None
           for fname in spl[:-1]:
@@ -118,49 +121,49 @@ class Command:
               var = Parcel._globals.get(fname)
             else:
               var = getattr(var, fname, None)
-              
+
             if var is None:
               break
-            
+
           if var != None:
             comp = self._get_comp(obj=var, pre=spl[-1])
-              
+
       # var
-      elif text: 
+      elif text:
           replace_l = len(text)
           comp = self._get_comp(obj=None, pre=text)
-          
+
       if comp:
         comp.sort()
-        
+
         edt.complete('\n'.join(comp), replace_l, replace_r)
-          
-      
+
+
   def _get_comp(self, obj, pre):
     dir_res = dir(obj)  if obj != None else  Parcel._globals
-    
+
     comp = []
     for name in dir_res:
         if not name.startswith(pre)   or name.startswith('__'):
           continue
-          
+
         if obj != None:
-          f = getattr(obj, name)  
+          f = getattr(obj, name)
         else:
           f = Parcel._globals.get(name)
-            
+
         if not add_func_params  or not callable(f):
           comp.append(prefix +'|'+ name)
-            
+
         else:
           try:
-            # ArgSpec( args=['id_menu', 'id_action', 'command', 'caption', 'index', 'hotkey', 'tag'], 
-            #          varargs=None, keywords=None, 
+            # ArgSpec( args=['id_menu', 'id_action', 'command', 'caption', 'index', 'hotkey', 'tag'],
+            #          varargs=None, keywords=None,
             #          defaults=('', '', -1, '', ''))
             spec = inspect.getfullargspec(f)
           except TypeError:
             spec = None
-            
+
           if spec:
               sargs = []
               arglen = len(spec.args) if spec.args else 0
@@ -169,17 +172,17 @@ class Command:
               for i in range(arglen):
                 if i == 0 and spec.args[i] == 'self':
                   continue
-                  
+
                 if i < noargs:
                   sargs.append(spec.args[i])
                 else:
                   sargs.append(spec.args[i] +'='+ spec.defaults[i-noargs].__repr__())
-              
+
               comp.append('{0}|{1}|({2})'.format(prefix, name, ", ".join(sargs)))
           else:
               comp.append(prefix +'|'+ name)
-          
+
     return comp
-    
+
 class Parcel:
   _globals = None
